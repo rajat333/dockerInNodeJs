f2aa177cbaa7725adc10727e001f7d9888145ebf
Rajat Khurana <rajatkhuranak@gmail.com>
2018-10-15 23:11:59 +0530

    First build

---
+++ configrations/applicationMessage.js
@@ -0,0 +1,18 @@
+
+var Message = {
+
+        Error:{
+            InValid:'Error Occur',
+        },
+        Success:{
+            Succ: 'Success Occur',
+        },
+        InternalAppMessage:{
+            InternalServerError: 'App.Internal.Server.Error'
+        }
+
+}
+module.exports = {
+ 
+    Message: Message
+}   
